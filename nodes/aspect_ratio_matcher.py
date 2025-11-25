c5ab823b80160d0227a90bcfb1eaf8a016d23c1c
xuminglong <494462498@qq.com>
2025-11-25 14:27:50 +0800

    add aspect ration matcher for banana pro

--- nodes/aspect_ratio_matcher.py
+++ nodes/aspect_ratio_matcher.py
@@ -19,6 +19,46 @@ QWEN_IMAGE_ASPECT_RATIOS = {
     "3:4": (1140, 1472)
 }
 
+BANANA_PRO_ASPECT_RATIOS = {
+    "1K": {
+        "1:1": (1024, 1024),
+        "4:3": (1200, 896),
+        "3:4": (896, 1200),
+        "2:3": (848, 1264),
+        "3:2": (1264, 848),
+        "9:16": (768, 1376),
+        "16:9": (1376, 768),
+        "4:5": (928, 1152),
+        "5:4": (1152, 928),
+        "21:9": (1584, 672)
+    },
+    "2K": {
+        "1:1": (2048, 2048),
+        "4:3": (2400, 1792),
+        "3:4": (1792, 2400),
+        "2:3": (1696, 2528),
+        "3:2": (2528, 1696),
+        "9:16": (1536, 2752),
+        "16:9": (2752, 1536),
+        "4:5": (1856, 2304),
+        "5:4": (2304, 1856),
+        "21:9": (3168, 1344)
+    },
+    "4K": {
+        "1:1": (4096, 4096),
+        "4:3": (4800, 3584),
+        "3:4": (3584, 4800),
+        "2:3": (3392, 5056),
+        "3:2": (5056, 3392),
+        "9:16": (3072, 5504),
+        "16:9": (5504, 3072),
+        "4:5": (3712, 4608),
+        "5:4": (4608, 3712),
+        "21:9": (6336, 2688)
+    }
+}
+
+
 class aspect_ratio_matcher:
     """
     ComfyUI节点：根据输入图像和选择的模式，计算最接近的标准宽高比，并输出对应分辨率
@@ -31,7 +71,8 @@ class aspect_ratio_matcher:
         return {
             "required": {
                 "image": ("IMAGE",),  # 输入图像
-                "mode": (["banana", "qwen_image"],),  # 模式选择
+                "mode": (["banana", "banana_pro", "qwen_image"],),  # 模式选择
+                "banana_pro_resolution":(["1K", "2K", "4K"],),
             }
         }
     
@@ -60,10 +101,12 @@ class aspect_ratio_matcher:
         
         return closest_ratio
     
-    def match_aspect_ratio(self, image, mode):
+    def match_aspect_ratio(self, image, mode, banana_pro_resolution):
         # 根据选择的模式获取对应的宽高比字典
         if mode == "banana":
             aspect_ratios = BANANA_ASPECT_RATIOS
+        elif mode == "banana_pro":
+            aspect_ratios = BANANA_PRO_ASPECT_RATIOS
         else:  # qwen_image模式
             aspect_ratios = QWEN_IMAGE_ASPECT_RATIOS
         
@@ -71,8 +114,13 @@ class aspect_ratio_matcher:
         batch_size, height, width, channels = image.shape
         
         input_ratio = self.calculate_aspect_ratio(width, height)
-        closest_ratio = self.find_closest_aspect_ratio(input_ratio, aspect_ratios)
-        target_width, target_height = aspect_ratios[closest_ratio]
+        
+        if mode == "banana_pro":
+            closest_ratio = self.find_closest_aspect_ratio(input_ratio, aspect_ratios[banana_pro_resolution])
+            target_width, target_height = aspect_ratios[banana_pro_resolution][closest_ratio]
+        else:
+            closest_ratio = self.find_closest_aspect_ratio(input_ratio, aspect_ratios)
+            target_width, target_height = aspect_ratios[closest_ratio]
         
         return (closest_ratio, target_width, target_height)
 
